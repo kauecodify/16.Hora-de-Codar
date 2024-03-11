fb946d3cede37a49b2065dd79048c4b9c7fc1fcd
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-03-11 15:46:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+var land_rover_vogue = "fusca"
+var nome = prompt("Informe seu nome:");
+alert(Olá, "nome do usuário", seja bem vindo!);
+console.log("Olá, " + nome);
+document.write("Olá, " + nome);
+
+var nome = prompt("Informe seu nome:");
+var idade = prompt("Informe sua idade:");
+alert("Olá" + nome + "você tem" + idade + "anos.");
+console.log("Olá, " + nome);
+document.write("Olá, " + nome);
+
+*//medindo um retângulo
+"altura" = a
+"base" = b
+"retângulo" = r
+r = b.a
+System.Print.Out; alert("O valor do retângulo é: " + r)
+
+*//quadrado
+"lado" = a
+"quadrado" = q
+q = a.a = a**a
+System.Print.Out; alert("O valor do quadrado é: " + q)
+
+*//losango
+"diagonal maior" = da
+"diagonal menor" = dm
+"losango" = l
+l = da+dm/2
+System.Print.Out; alert("O valor do losango é: " + l)
+
+
+*//Trapézio
+"base maior" = Ba
+"base menor" = bm
+"altura" = h
+"trapézio" = t
+t = ba+bm.h/2
+System.Out.Print; alert("O valor do trapézio é: " + p);
+
+*//paralelogramo
+"altura" = a
+"base" = b
+"paralelogramo" = p
+p = b.a
+System.Out.Print; alert("O valor do paralelogramo é: " + p);
+
+
+*//Triângulo
+"altura" = a
+"base" = b
+"triângulo" = t
+t = b.a/2
+System.Out.Print; alert("O valor do triângulo é: " + t);
+
+
+*//Círculo
+"pi" = PI
+"raio"= r
+"círculo" = c
+c=PI.r**
+System.Out.Print; alert("O valor do círculo é: " + c);
